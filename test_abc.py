cc2f202a800559314c0c4f43a93447f9d930427d
Unnati06-lab <patilunnati2006@gmail.com>
2025-11-24 17:02:42 +0530

    Update test_abc.py

--- test_abc.py
+++ test_abc.py
@@ -1,19 +1,9 @@
-import sys
 from abc import add
 
-# Take values from command-line arguments
-a1 = eval(sys.argv[1])
-b1 = eval(sys.argv[2])
-a2 = eval(sys.argv[3])
-b2 = eval(sys.argv[4])
-a3 = eval(sys.argv[5])
-b3 = eval(sys.argv[6])
-
 def test_add_positive_numbers():
-    assert add(a1, b1) == eval(sys.argv[7])
+    assert add(2, 3) == 5
 
 def test_add_negative_numbers():
-    assert add(a2, b2) == eval(sys.argv[8])
-
+    assert add(-4, -6) == -10
 def test_add_zero():
-    assert add(a3, b3) == eval(sys.argv[9])
+    assert add(0, 5) == 5
